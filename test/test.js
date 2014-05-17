4bdaf26d40ce2901602095ea69690da7cae20f84
Matthew Kastor <matthewkastor@gmail.com>
2014-05-16 22:18:46 -0400

    Jasmine has cool asynchronous testing with "done"

--- test/test.js
+++ test/test.js
@@ -1,35 +1,49 @@
+/*jslint
+    node: true,
+    nomen: true,
+    white: true
+*/
+/*globals
+    describe,
+    it,
+    expect
+*/
 var Nodewiki = require('../nodewiki');
-var assert = require('assert');
-
 var fixtures = require('path').join(__dirname, 'fixtures');
+var app = {};
 
-
-describe('app instantiation', function(){
-  it('should give an error setting rootDir to a directory that doesn\'t exist', function(){
-    assert.throws(function(){
-      var app = new Nodewiki({rootDir: __dirname + '/doesntexist'})
+describe('app instantiation', function () {
+    "use strict";
+    it('should give an error setting rootDir to a directory that doesn\'t exist', function () {
+        expect(function () {
+            app = new Nodewiki({
+                    rootDir : __dirname + '/doesntexist'
+                });
+        }).toThrow();
     });
-
-  });
 });
 
-describe('directory listings', function(){
-  it('should list files', function(){
-    var app = new Nodewiki({rootDir: fixtures});
-
-    app.listFiles('/', function(err, files){
-      assert.equal(files.length, 1);
-      assert.equal(files[0].name, 'test.md');
-      assert.equal(files[0].type, 'file');
-    });
-  });
-
-  it('should give an error when requesting a listing outside of rootDir', function(){
-    var app = new Nodewiki({rootDir: fixtures});
-
-    app.listFiles('../../', function(files){
-      assert.equal(typeof(files), 'string');
+describe('directory listings', function () {
+    "use strict";
+    app = new Nodewiki({
+            rootDir : fixtures
+        });
+    it('should list files', function (done) {
+        /*jslint unparam: true */
+        app.listFiles('/', function (err, files) {
+            expect(files.length).toEqual(1);
+            expect(files[0].name).toEqual('test.md');
+            expect(files[0].type).toEqual('file');
+            done();
+        });
+        /*jslint unparam: false */
     });
 
-  });
+    it('should give an error when requesting a listing outside of rootDir', function (done) {
+        app.listFiles('../../', function (files) {
+            expect(typeof files).toEqual('string');
+            done();
+        });
+
+    });
 });
